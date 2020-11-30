5d1382314f306115d66be2e4a0b1c863b8b47192
Ane Acevedo <ane.acevedo@gmail.com>
2020-11-30 16:21:03 -0300

    Repositorio completo 30/11

---
+++ enlaces.md
@@ -0,0 +1,4 @@
+[Google] http://www.google.cl
+http://www.facebook.com Facebook
+http://google.com/hola
+http://www.instagram.com
--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+const log = console.log;
+
+
+//Archivo Markdown
+const mdLink = (path) => {
+    if (path.slice(-3) == '.md') {
+        log(path('Archivo encontrado'));
+    } else {
+        log('Este archivo no es del tipo Markdown');
+    };
+    return mdLink;
 };
+
+// Lectura de archivos
+
+const readingFile = (file => {
+    let promise = new Promise((resolve, reject) => {
+        fs.readFile(file, 'utf-8', (error, data) => {
+            if (error) {
+                reject(new Error('Archivo no encontrado'));
+            }
+            resolve(data);
+        });
+    });
+    return promise;
+});
+
+module.exports = readingFile;
